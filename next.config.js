1171e9887b85dc434b883766b078964cc3e066e9
jamie0xgitc0decat <syloag@connect.ust.kh>
2022-03-11 20:14:17 +0800

    add cdn

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd
+    ? "https://cdn.statically.io/gh/covid-lab-hk/covid-lab-hk.github.io"
+    : "",
   reactStrictMode: true,
   distDir: "build",
 };
