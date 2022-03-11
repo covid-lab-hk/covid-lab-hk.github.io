b33f47854ec16a99345e53a092490ea0323e679c
jamie0xgitc0decat <syloag@connect.ust.kh>
2022-03-11 20:23:29 +0800

    try to fix github page issue

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? "/covid-lab-hk.github.io/" : "",
+  assetPrefix: isProd ? "/covid-lab-hk.github.io/gh-pages/" : "",
   reactStrictMode: true,
   distDir: "build",
 };
